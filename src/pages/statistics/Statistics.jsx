fcdde935a72fc493aac045f35ac3b3c79f6b5b40
shirtol <shirzahavi7@gmail.com>
2022-06-07 17:11:58 +0300

    Added chart to statistics page

--- src/pages/statistics/Statistics.jsx
+++ src/pages/statistics/Statistics.jsx
@@ -1,7 +1,87 @@
 import React from "react";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+import Months from "../../utils/months";
+import { useWorkoutsPerMonth } from "../../context/WorkoutsPerMonthContext";
 
 const Statistics = () => {
-    return <div>Statistics</div>;
+    // const currDate = new Date();
+    // const currYear = currDate.getFullYear()
+    const { allWorkoutsPerMonth } = useWorkoutsPerMonth();
+
+    console.log(allWorkoutsPerMonth);
+
+    const getAllActiveMonths = () =>
+        allWorkoutsPerMonth.map((workout) => ({
+            month: workout.month,
+            numOfWorkouts: workout.numOfWorkouts,
+        }));
+
+    const getArrayOfWorkoutCounts = () => {
+        const monthsWorkoutsMap = getAllActiveMonths();
+        const arrOfWorkoutCounts = [];
+        Object.keys(Months).forEach((month) => {
+            const foundObject = monthsWorkoutsMap.find(
+                (monthWorkout) => monthWorkout.month === month
+            );
+            if (foundObject) {
+                arrOfWorkoutCounts.push(foundObject.numOfWorkouts);
+            } else {
+                arrOfWorkoutCounts.push(0);
+            }
+        });
+        return arrOfWorkoutCounts;
+    };
+
+    const chartData = getArrayOfWorkoutCounts();
+
+    const data = {
+        labels: Object.keys(Months),
+        datasets: [
+            {
+                label: 2022,
+                data: chartData,
+                backgroundColor: [
+                    "rgba(247, 184, 38, 0.6)",
+                    // "rgba(255, 255, 255, 0.6)",
+                    // "rgba(255, 255, 255, 0.6)",
+                ],
+                borderWidth: 2,
+            },
+        ],
+    };
+
+    return <Line data={data} />;
+};
+
+export const options = {
+    responsive: true,
+    interaction: {
+        mode: "index",
+        intersect: false,
+    },
+    stacked: false,
+    plugins: {
+        title: {
+            display: true,
+            text: "Chart.js Line Chart - Multi Axis",
+        },
+    },
+    scales: {
+        y: {
+            type: "linear",
+            display: true,
+            position: "left",
+        },
+        y1: {
+            type: "linear",
+            display: true,
+            position: "right",
+            grid: {
+                drawOnChartArea: false,
+            },
+        },
+    },
 };
 
 export default Statistics;
--- src/pages/watchPlaylist/WatchPlaylist.jsx
+++ src/pages/watchPlaylist/WatchPlaylist.jsx
@@ -4,13 +4,22 @@ import { StyledPlaylistContainer } from "../createPlaylist/StyledPlaylistContain
 import VideoItem from "../../components/videoItem/VideoItem";
 import { StyledFlexWrapper } from "../../components/wrappers/flexWrapper/StyledFlexWrapper";
 import "./watchPlaylist.css";
+import { Button } from "@mui/material";
+import { getKeyByValue } from "../../utils/utils";
+import Months from "../../utils/months";
+import { useWorkoutsPerMonth } from "../../context/WorkoutsPerMonthContext";
+import { useFirebase } from "../../context/FirebaseContext";
+import { setDocument } from "../../utils/firebaseUtils";
+import { v4 as uuid } from "uuid";
 
 const WatchPlaylist = ({ location }) => {
     const playlistItem = location.item;
-
     const [currVideoPlaying, setCurrVideoPlaying] = useState(
         playlistItem.videos[0]
     );
+    const { allWorkoutsPerMonth, setAllWorkoutsPerMonth } =
+        useWorkoutsPerMonth();
+    const { db, currentUser } = useFirebase();
 
     const videoSrc = `https://www.youtube.com/embed/${playlistItem.videos[0].id}`;
 
@@ -35,6 +44,39 @@ const WatchPlaylist = ({ location }) => {
         setCurrVideoPlaying(video);
     };
 
+    const onEndWorkoutClicked = () => {
+        const currDate = new Date();
+        const monthNum = currDate.getMonth();
+        const stringyMonth = getKeyByValue(Months, monthNum);
+        const currYear = currDate.getFullYear();
+        const workoutObj = allWorkoutsPerMonth.find(
+            (workoutEl) =>
+                workoutEl?.month === stringyMonth &&
+                workoutEl?.year === currYear
+        );
+        console.log(workoutObj);
+        if (workoutObj !== undefined) {
+            workoutObj.numOfWorkouts += 1;
+            const newArrayOfWorkoutsCounts = allWorkoutsPerMonth.map(
+                (workoutEl) =>
+                    workoutEl === workoutObj ? workoutObj : workoutEl
+            );
+            setAllWorkoutsPerMonth(newArrayOfWorkoutsCounts);
+            setDocument(db, "workoutsPerMonth", workoutObj, workoutObj.id);
+        } else {
+            const id = uuid();
+            const newObj = {
+                month: stringyMonth,
+                numOfWorkouts: 1,
+                year: currYear,
+                owner: currentUser.uid,
+                id: id,
+            };
+            setAllWorkoutsPerMonth([...allWorkoutsPerMonth, newObj]);
+            setDocument(db, "workoutsPerMonth", newObj, id);
+        }
+    };
+
     const onCurrVideoEnd = () => {
         const idxOfVideo = playlistItem.videos.indexOf(currVideoPlaying);
         if (idxOfVideo < playlistItem.videos.length - 1) {
@@ -43,25 +85,28 @@ const WatchPlaylist = ({ location }) => {
     };
 
     return (
-        <StyledFlexWrapper>
-            <StyledFlexWrapper
-                justifyContent="space-between"
-                width="90%"
-                height="100vh"
-                flexDirectionTablet="column-reverse"
-            >
-                <StyledPlaylistContainer position="static">
-                    {displayPlaylistVideos()}
-                </StyledPlaylistContainer>
-                <div className="youtube-wrapper">
-                    <YouTube
-                        videoId={currVideoPlaying.id}
-                        onEnd={onCurrVideoEnd}
-                        opts={{ playerVars: { autoplay: 1 } }}
-                    ></YouTube>
-                </div>
+        <>
+            <Button onClick={onEndWorkoutClicked}>End Workout</Button>
+            <StyledFlexWrapper>
+                <StyledFlexWrapper
+                    justifyContent="space-between"
+                    width="90%"
+                    height="100vh"
+                    flexDirectionTablet="column-reverse"
+                >
+                    <StyledPlaylistContainer position="static">
+                        {displayPlaylistVideos()}
+                    </StyledPlaylistContainer>
+                    <div className="youtube-wrapper">
+                        <YouTube
+                            videoId={currVideoPlaying.id}
+                            onEnd={onCurrVideoEnd}
+                            opts={{ playerVars: { autoplay: 1 } }}
+                        ></YouTube>
+                    </div>
+                </StyledFlexWrapper>
             </StyledFlexWrapper>
-        </StyledFlexWrapper>
+        </>
     );
 };
 
