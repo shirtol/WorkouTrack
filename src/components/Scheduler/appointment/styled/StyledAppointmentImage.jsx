27390d45a4700a2895d06c97a3a4a4c7c66734ab
shirtol <shirzahavi7@gmail.com>
2022-06-08 15:54:47 +0300

    Fixed appointment box layout

--- src/components/Scheduler/appointment/styled/StyledAppointmentImage.jsx
+++ src/components/Scheduler/appointment/styled/StyledAppointmentImage.jsx
@@ -1,7 +1,16 @@
 import styled from "styled-components";
+import device from "../../../../utils/mediaQuerySizes";
 
 export const StyledAppointmentImage = styled.img`
-    width: 50px;
-    height: 50px;
+    width: 5rem;
+    height: 5rem;
     align-self: center;
+    @media ${device.laptop} {
+        width: 4rem;
+        height: 4rem;
+    }
+    @media ${device.mobileL} {
+        width: 3rem;
+        height: 3rem;
+    }
 `;
